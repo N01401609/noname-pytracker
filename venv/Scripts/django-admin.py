93600be4d94a46797a51134907e704307e9fa819
Alexa Perez <alexapg8@gmail.com>
2020-05-28 15:35:19 -0400

    'users'

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\alexa\Desktop\Web Development 3rd Semester\Web Project\NoName\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
